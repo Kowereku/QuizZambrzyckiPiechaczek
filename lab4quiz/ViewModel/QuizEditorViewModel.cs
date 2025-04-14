d3327c96faf903c3958e86fd54c62fe19d97f533
KarolPiechaczek <piechaczekkarol1@gmail.com>
2025-04-14 20:15:11 +0200

    ViewModel update

---
+++ lab4quiz/ViewModel/QuizEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4quiz.ViewModel
+{
+    class QuizEditorViewModel
+    {
+        public static implicit operator ObservableObject(QuizEditorViewModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lab4quiz/ViewModel/QuizSolverViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4quiz.ViewModel
+{
+    class QuizSolverViewModel
+    {
+        public static implicit operator ObservableObject(QuizSolverViewModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
