6d185acb7139980f8e6a5b90cf0a8909ffd03e49
coverek <pawelzambrzycki03@gmail.com>
2025-04-14 19:47:36 +0200

    Added basic AES encryption and decryption according to online tutorial

---
+++ lab4quiz/AESCipher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4quiz
+{
+    public class AESCipher
+    {
+        private static readonly string EncryptionKey = "kochamdotNET2115";
+
+        private static byte[] EncryptStringToBytes_Aes(string plainText, string key)
+        {
+            using Aes aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.UTF8.GetBytes(key);
+            aesAlg.IV = new byte[16];
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            using var msEncrypt = new MemoryStream();
+            using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+            using (var swEncrypt = new StreamWriter(csEncrypt))
+                swEncrypt.Write(plainText);
+            return msEncrypt.ToArray();
+        }
+
+        private static string DecryptStringFromBytes_Aes(byte[] cipherText, string key)
+        {
+            using Aes aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.UTF8.GetBytes(key);
+            aesAlg.IV = new byte[16];
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+            using var msDecrypt = new MemoryStream(cipherText);
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+            return srDecrypt.ReadToEnd();
+        }
+    }
+}
